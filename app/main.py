65d92bc6958df7a2ff85091e5ad1b636b438a7bf
Jonathan Carlson <JonathanCarlson3712@Hotmail.com>
2026-04-25 11:52:45 -0500

    Fixed server errors. Can now view on any device on network.

--- app/main.py
+++ app/main.py
@@ -14,8 +14,9 @@ MEDIA_ROOT = BASE_DIR / "media" / "galleries"
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "http://192.168.1.249:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
